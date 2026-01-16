13cdbf293b682cc0afa391317e6e1e6afe7fbe6f
Dan Morris <dmorris@morrisdev.com>
2026-01-15 20:08:16 -0800

    updated env

--- src/app/services/council-directory.service.ts
+++ src/app/services/council-directory.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, shareReplay } from 'rxjs';
 import { Council } from '../models/council.model';
+import { Agent } from '../models/agent.model';
 
  
 @Injectable({
   providedIn: 'root'
 })
 export class CouncilDirectoryService {
+    private councils$?: Observable<Council[]>;
 
   constructor(private http: HttpClient) { }
 
   getCouncils(): Observable<Council[]> {
-    return this.http.get<Council[]>('/data/councils.json');
+    if(!this.councils$) {
+      this.councils$ = this.http.get<Council[]>('/data/councils.json').pipe(
+        shareReplay(1)
+      );
+    }
+    return this.councils$;
   }
 }
